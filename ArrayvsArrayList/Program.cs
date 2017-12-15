0f18d516a1ef2f87cc236e4ce5474bd472c88e35
CSharpFri <CSharpFri@CLASS-2-11>
2017-12-15 21:18:31 +0100

    fin

--- ArrayvsArrayList/Program.cs
+++ ArrayvsArrayList/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,52 @@ namespace ArrayvsArrayList
     {
         static void Main(string[] args)
         {
+            //const int miliarda = 100000000;
+            //int[] miliardaPole = new int[miliarda];
+            //ArrayList miliardaAlist = new ArrayList();
+
+            //Random r = new Random();
+
+            //Stopwatch stopky = new Stopwatch();
+            //stopky.Start();
+
+            //for (int i = 0; i < miliardaPole.Length; i++)
+            //{
+            //    miliardaPole[i] = r.Next();
+            //}
+            //stopky.Stop();
+            //Console.WriteLine("naplnenie pola{0}", stopky.Elapsed);
+
+            //stopky.Restart();
+            //for(int i = 0; i < miliarda; i++)
+            //{
+            //    miliardaAlist.Add(r.Next());
+            //}
+            //stopky.Stop();
+            //Console.WriteLine("naplnenie Arraylistu {0}", stopky.Elapsed);
+
+            //stopky.Restart();
+            //Array.Sort(miliardaPole);
+            //stopky.Stop();
+            //Console.WriteLine("sort array{0}", stopky.Elapsed);
+
+            //stopky.Restart();
+            //miliardaAlist.Sort();
+            //stopky.Stop();
+            //Console.WriteLine("sort alist {0}", stopky.Elapsed);
+
+            string vstup = "bgvhh776";
+            try
+            {
+                int cislo = int.Parse(vstup);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
